581741bacc5aaf1fb0776cb0b0e25d895d31640f
nstern <sirocknick@gmail.com>
2017-07-06 16:47:18 -0700

    Moved amountFor(Rental rental) to Rental class since it doesn't use any information from Customer.

--- src/Customer.java
+++ src/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
         int frequentRenterPoints = 0;
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-            double thisAmount = amountFor(rental);
+            double thisAmount = rental.amountFor();
             frequentRenterPoints++;
             if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1) {
                 frequentRenterPoints++;
@@ -39,25 +39,4 @@ public class Customer {
         return result;
     }
 
-    private double amountFor(Rental rental) {
-        double thisAmount = 0;
-        switch (rental.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2) {
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
-    }
 }
